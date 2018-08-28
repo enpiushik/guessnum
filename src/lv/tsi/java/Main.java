f4330a1dda0102ffbf16b886434f342fba42c0ab
enpiushik <enpiushik@inbox.lv>
2018-08-28 20:47:11 +0300

    Initial commit

---
+++ src/lv/tsi/java/Main.java
@@ -0,0 +1,34 @@
+package lv.tsi.java;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    static Random rand = new Random();
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int myNum = rand.nextInt(100) + 1;
+        System.out.println(myNum);
+
+        boolean userLost = true;
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Try #" + i);
+            int userNum = scan.nextInt();
+
+            if (myNum < userNum) {
+                System.out.println("my number is smaller than yours");
+            } else if (myNum > userNum) {
+                System.out.println("my number is bigger than yours");
+            } else {
+                System.out.println("you won!!!");
+                userLost = false;
+                break;
+            }
+        }
+        if (userLost) {
+            System.out.println("you lost!!!");
+        }
+    }
+}
