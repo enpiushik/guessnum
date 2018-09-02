67f176989b7ba1bc6de3d9c57d407747968891f4
enpiushik <enpiushik@inbox.lv>
2018-09-02 19:23:53 +0300

    igra ugodaj chislo + D/Z (prodolzitj igru? vopros YES/NO)

--- src/lv/tsi/java/Main.java
+++ src/lv/tsi/java/Main.java
@@ -10,25 +10,29 @@ public class Main {
     public static void main(String[] args) {
         int myNum = rand.nextInt(100) + 1;
         System.out.println(myNum);
-
+        String answer;
         boolean userLost = true;
+        do {
+            for (int i = 1; i < 11; i++) {
+                System.out.println("Try #" + i);
+                int userNum = scan.nextInt();
 
-        for (int i = 1; i < 11; i++) {
-            System.out.println("Try #" + i);
-            int userNum = scan.nextInt();
-
-            if (myNum < userNum) {
-                System.out.println("my number is smaller than yours");
-            } else if (myNum > userNum) {
-                System.out.println("my number is bigger than yours");
-            } else {
-                System.out.println("you won!!!");
-                userLost = false;
-                break;
+                if (myNum < userNum) {
+                    System.out.println("my number is smaller than yours");
+                } else if (myNum > userNum) {
+                    System.out.println("my number is bigger than yours");
+                } else {
+                    System.out.println("you won!!!");
+                    userLost = false;
+                    break;
+                }
             }
-        }
-        if (userLost) {
-            System.out.println("you lost!!!");
-        }
+            if (userLost) {
+                System.out.println("you lost!!!");
+            }
+            System.out.print("new game? (Y/N)");
+            answer = scan.next();
+        } while (answer.equals("Y"));
+        System.out.println("goodbye!");
     }
 }
