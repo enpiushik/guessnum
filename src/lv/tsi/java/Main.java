d6c86c7c7dd01d01ae123b4c8fddd86aa46f12e7
enpiushik <enpiushik@inbox.lv>
2018-09-04 20:48:58 +0300

    final version: igra ugadaj chislo (guessnub); lesson 04.09.2018

--- src/lv/tsi/java/Main.java
+++ src/lv/tsi/java/Main.java
@@ -1,5 +1,6 @@
 package lv.tsi.java;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,31 +9,64 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String answer;
+        do {
         int myNum = rand.nextInt(100) + 1;
         System.out.println(myNum);
-        String answer;
         boolean userLost = true;
-        do {
-            for (int i = 1; i < 11; i++) {
+
+            for (int i = 1; i <= 10; i++) {
                 System.out.println("Try #" + i);
-                int userNum = scan.nextInt();
+                int userNum = askNum();
 
                 if (myNum < userNum) {
-                    System.out.println("my number is smaller than yours");
+                    System.out.println("My number is smaller than yours");
                 } else if (myNum > userNum) {
-                    System.out.println("my number is bigger than yours");
+                    System.out.println("My number is bigger than yours");
                 } else {
-                    System.out.println("you won!!!");
+                    System.out.println("You won!!!");
                     userLost = false;
                     break;
                 }
             }
             if (userLost) {
-                System.out.println("you lost!!!");
+                System.out.println("You lost!!!");
             }
-            System.out.print("new game? (Y/N)");
+            System.out.print("New game? (y/n)");
+            answer = askYN();
+        } while (answer.equals("y"));
+        System.out.println("Goodbye!");
+    }
+
+
+    static String askYN () {
+        String answer;
+        do {
             answer = scan.next();
-        } while (answer.equals("Y"));
-        System.out.println("goodbye!");
+            if (!answer.equals ("y") && !answer.equals("n")) {
+                System.out.println("You can enter only 'y' or 'n'");
+            } else {
+                return answer;
+            }
+        } while (true);
+    }
+
+
+    static int askNum (){
+        int answer;
+        do {
+            try {
+                answer = scan.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("This is not a number! Please, enter a number.");
+                scan.next();
+                continue;
+            }
+            if (answer < 1 || answer > 100) {
+                System.out.println("Please, enter a number between 1 and 100.");
+            } else {
+                return answer;
+            }
+        } while (true);
     }
 }
